b3eeff7ad026644d35af516a60aa6638a64d7bca
Ilya Shevtsov <shevtsov.i@inetra.ru>
2023-08-06 11:43:09 +0700

    added new module

---
+++ feature/navigation/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin)
+}
+
+android {
+    namespace = "com.example.feature.navigation"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+dependencies {
+
+    implementation(libs.android.core)
+    implementation(libs.appcompat)
+    implementation(libs.bundles.navigation)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.bundles.android.test)
+}
