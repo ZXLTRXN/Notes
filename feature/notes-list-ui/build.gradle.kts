557bc2109559c56ca126190eabebbe5bbcc1c8ce
Ilya Shevtsov <shevtsov.i@inetra.ru>
2023-08-11 14:21:37 +0700

    added new module

---
+++ feature/notes-list-ui/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin)
+}
+
+android {
+    namespace = "com.example.feature.notes_list_ui"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+dependencies {
+
+    implementation(libs.android.core)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+
+    implementation(libs.bundles.lifecycle)
+    implementation(libs.bundles.coroutines)
+
+    implementation(project(":core:common"))
+    implementation(project(":core:ui"))
+    implementation(project(":core:navigation"))
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.bundles.android.test)
+}
