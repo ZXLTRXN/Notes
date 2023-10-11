a1a26d314ee1423ee4eaf0838ad17c40450223dc
Ilya Shevtsov <shevtsov.i@inetra.ru>
2023-10-11 13:29:35 +0700

    examples of coroutines and AppDispatchers class

--- feature/notes-list/src/main/java/com/example/feature/notes_list/presentation/NotesListViewModel.kt
+++ feature/notes-list/src/main/java/com/example/feature/notes_list/presentation/NotesListViewModel.kt
@@ -3,10 +3,35 @@ package com.example.feature.notes_list.presentation
 import androidx.lifecycle.ViewModel
 import com.example.api.notes.domain.GetAllNotesUseCase
 import javax.inject.Inject
+import kotlin.random.Random
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
-class NotesListViewModel @Inject constructor(private val getAllNotes: GetAllNotesUseCase) :
+class NotesListViewModel @Inject constructor(
+    private val getAllNotes: GetAllNotesUseCase
+    ) :
     ViewModel() {
     fun test() {
-        println(getAllNotes.get())
+        runBlocking {
+
+            repeat(100) {
+                // будет выполняться асинхронно
+                launch {
+                    doWork(it.toString())
+                }
+            }
+
+            repeat(100) {
+                // без корутины (launch, async) будет выполняться последовательно
+                doWork(it.toString())
+            }
+
+        }
+    }
+
+    private suspend fun doWork(text: String) {
+        delay(Random(2).nextLong(1500))
+        println(text)
     }
 }
